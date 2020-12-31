ffe9b9abd60bff6a33cb3e1a1ec1cd685ef76f98
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-31 15:31:55 -0800

    prettify files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-	purge: ['./pages/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-	  extend: {
-		colors: {},
-	  },
-	  fontFamily: {
-		sans: `'Space Grotesk', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
-	  },
-	},
-	variants: {
-	  extend: {},
-	},
-	plugins: [],
-  }
-  
+  purge: ['./pages/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {},
+    },
+    fontFamily: {
+      sans: `'Space Grotesk', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
