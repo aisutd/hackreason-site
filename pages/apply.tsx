f8788b089196c8bcc8c273beb878757bec6c7efa
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-31 19:21:55 -0800

    Create /apply redirect

---
+++ pages/apply.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+import Head from 'next/head'
+
+const Apply: FC = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+      <Head>
+        <meta httpEquiv='refresh' content='0; url=https://form.typeform.com/to/voPGPt4j' />
+      </Head>
+      <h1 className='font font-medium'>Loading application...</h1>
+    </div>
+  )
+}
+
+export default Apply
