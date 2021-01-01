63efbcfa4edc630f5dca2ef1d62f1c11b4b4676e
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-31 19:20:43 -0800

    Add background images, assets, and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        capri: '#00B9FF',
+        darkcapri: '#008ee0',
+      },
     },
     fontFamily: {
       sans: `'Space Grotesk', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
