a56360feaaf194c5d914322ad7fd97e3962113bc
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-01-01 14:15:16 -0800

    Update FAQ

--- components/faq.tsx
+++ components/faq.tsx
@@ -7,9 +7,18 @@ const FAQ: FC = () => {
       <h1 className='text-4xl font-bold text-center mb-8'>FAQs</h1>
       <FAQCard
         question={'What is s(CASP)?'}
-        answer='s(CASP) is a system developed at UT Dallas for automating common sense reasoning.
-	  Professor Gupta will give two 2-hour tutorials to provide the technological background,
-	  including on how to use the s(CASP) system, a week prior to the event.'
+        answer={
+          <p>
+            s(CASP) is a system developed by UT Dallas in collaboration with{' '}
+            <a href='//software.imdea.org/~joaquin.arias/' target='_blank' rel='noreferrer'>
+              IMDEA, Spain
+            </a>{' '}
+            for automating common sense reasoning. Professor Gupta will give two 2-hour tutorials to
+            provide the technological background, including on how to use the s(CASP) system, a week
+            prior to the event. The team in Spain will be running a parallel hackathon at the same
+            time, and Spanish students will be joining for the opening ceremony and other workshops.
+          </p>
+        }
       />
       <FAQCard
         question={'Who can participate?'}
@@ -31,17 +40,38 @@ $600, second place will win $400, and third place will win $200.'
           <p>
             The first Automated Reasoning hackathon at UTD was organized in 2016 in a similar manner
             and sponsored by Prof. Gopal Gupta. You can see the projects{' '}
-            <a href='https://hackai16.devpost.com/project-gallery'>here</a>!
+            <a href='https://hackai16.devpost.com/project-gallery' target='_blank' rel='noreferrer'>
+              here
+            </a>
+            !
+          </p>
+        }
+      />
+      <FAQCard
+        question='What is the Amazon Alexa Socialbot Challenge?'
+        answer={
+          <p>
+            The Alexa Prize, launched in 2016, is a competition for university students dedicated to
+            advancing the field of conversational AI. Teams are challenged to design socialbots that
+            Alexa customers can interact with via Alexa-enabled devices. UT Dallas has placed in one
+            of the top <span className='font-semibold'>9 teams worldwide</span> for this
+            competition! You can learn more about the Amazon Alexa Socialbot challenge{' '}
+            <a href='https://tinyurl.com/amazon-alexa-challenge' target='_blank' rel='noreferrer'>
+              here.
+            </a>
           </p>
         }
       />
       <FAQCard
         question='What is "common sense reasoning"?'
         answer={
-          <span>
+          <p>
             You can watch a video introduction on the topic{' '}
-            <a href='https://www.youtube.com/watch?v=IHE63fxpHCg'>here</a>.
-          </span>
+            <a href='https://www.youtube.com/watch?v=IHE63fxpHCg' target='_blank' rel='noreferrer'>
+              here
+            </a>
+            .
+          </p>
         }
       />
     </div>
