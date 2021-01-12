931371ee9c08226d9e99a0099a3061bc9524c410
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-01-11 20:48:54 -0800

    minor text fix: Change text on Welcome page

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -10,7 +10,7 @@ const Welcome: FC = () => {
           content='0; url="https://www.notion.so/HackReason-Welcome-Packet-bce24bd6dbd34d70aa89c1f6b1f8ea3d"'
         />
       </Head>
-      <h1 className='font font-medium'>Loading Discord...</h1>
+      <h1 className='font font-medium'>Loading welcome packet...</h1>
     </div>
   )
 }
